4fb5b9361df8aab6481c058fe7dd28ece4385ec5
LorenzoBonanni <lorenzopuntobonanni@gmail.com>
2023-06-21 09:24:40 +0200

    fixed find and union

--- solver.py
+++ solver.py
@@ -8,17 +8,22 @@ class CongruenceClosureAlgorithm:
         self.f_minus = f_minus
 
     def find(self, id1):
-        if id1 == self.id_to_node[id1].find_id:
+        n = self.id_to_node[id1]
+        if id1 == n.find_id:
             return id1
         else:
-            self.id_to_node[id1].find()
+            return self.find(n.find_id)
 
     def union(self, id1, id2):
         n1 = self.id_to_node[self.find(id1)]
         n2 = self.id_to_node[self.find(id2)]
         n1.find_id = n2.find_id
-        n2.ccpar = n1.ccpar + n2.ccpar
-        n1.ccpar = None
+        if len(n1.ccpar) < len(n2.ccpar):
+            n2.ccpar = n1.ccpar + n2.ccpar
+            n1.ccpar = []
+        else:
+            n1.ccpar = n1.ccpar + n2.ccpar
+            n2.ccpar = []
 
     def ccpar(self, id1):
         return self.id_to_node[self.find(id1)].ccpar
@@ -32,6 +37,7 @@ class CongruenceClosureAlgorithm:
 
     def merge(self, id1: int, id2: int):
         if self.find(id1) != self.find(id2):
+            self.union(id1, id2)
             p1 = self.ccpar(id1)
             p2 = self.ccpar(id2)
             for t1, t2 in itertools.product(p1, p2):
